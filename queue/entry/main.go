a96bee8c31009d17f564bd1729628bef1c74009a
ShuwenHe <1201220707@pku.edu.cn>
2021-02-23 21:17:52 +0800

    feat: git push origin main

--- queue/entry/main.go
+++ queue/entry/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/shuwenhe/shuwen-go1/queue"
+)
+
 func main() {
-	q := Queue()
+	q := queue.Queue{1}
+	q.Push(2)
+	q.Push(3)
+	fmt.Println("q = ", q)
+	q.Pop()
+	q.Pop()
+	fmt.Println("q = ", q)
+	fmt.Println(q.IsEmpty())
+	q.Pop()
+	fmt.Println(q.IsEmpty())
 }
